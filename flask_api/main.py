f3bc06b1e28ec843d9c3988189854d1783d0330f
mbewustanley <mbewustanley@gmail.com>
2025-12-06 09:12:45 +0100

    corrected flask code for testing on postman

--- flask_api/main.py
+++ flask_api/main.py
@@ -21,23 +21,24 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+
+
 def preprocess_comment(comment):
-    """Apply preprocessing transformations to a comment"""
+    """Apply preprocessing transformations to a comment."""
     try:
-        
-        # convert to lowercase
+        # Convert to lowercase
         comment = comment.lower()
 
-        # remove trailing and leading whitespaces
+        # Remove trailing and leading whitespaces
         comment = comment.strip()
 
-        # remove newline characters
+        # Remove newline characters
         comment = re.sub(r'\n', ' ', comment)
 
-        #remove non-alphanumeric characters, except punctuation
+        # Remove non-alphanumeric characters, except punctuation
         comment = re.sub(r'[^A-Za-z0-9\s!?.,]', '', comment)
 
-        #remove stopwords but retain important ones for sentiment analysis
+        # Remove stopwords but retain important ones for sentiment analysis
         stop_words = set(stopwords.words('english')) - {'not', 'but', 'however', 'no', 'yet'}
         comment = ' '.join([word for word in comment.split() if word not in stop_words])
 
@@ -47,61 +48,75 @@ def preprocess_comment(comment):
 
         return comment
     except Exception as e:
-        print(f'Error in preprocessing comment: {e}')
+        print(f"Error in preprocessing comment: {e}")
         return comment
-    
 
 # Load the model and vectorizer from the model registry and local storage
 def load_model_and_vectorizer(model_name, model_version, vectorizer_path):
     #set MLFlow tracking URI to your server
-    mlflow.set_tracking_uri("http://ec2-13-246-237-29.af-south-1.compute.amazonaws.com:5000/")
+    mlflow.set_tracking_uri("http://ec2-13-247-106-239.af-south-1.compute.amazonaws.com:5000/")
     client = MlflowClient()
 
     model_uri = f"models:/{model_name}/{model_version}"
-    model = mlflow.pyfunc.load_model(model_uri)
+    model = mlflow.sklearn.load_model(model_uri) # sklearn <> pyfunc
 
     with open(vectorizer_path, 'rb') as file:
         vectorizer = pickle.load(file)
     return model, vectorizer
 
+#def load_model(model_path, vectorizer_path):
+    """Load the trained model."""
+    try:
+        with open(model_path, 'rb') as file:
+            model = pickle.load(file)
+        
+        with open(vectorizer_path, 'rb') as file:
+            vectorizer = pickle.load(file)
+      
+        return model, vectorizer
+    except Exception as e:
+        raise
+
 
 #initialize model and vectorizer
 model, vectorizer = load_model_and_vectorizer("yt_chrome_plugin_model", "2", "./tfidf_vectorizer.pkl")
 
+
 #initialize first(default) route of flask api to test
 @app.route('/')
 def home():
-    return "Welcome to our flask api"
+    return "Welcome to Stanley's flask api"
+
+
 
 # predict route to test api on postman
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     comments = data.get('comments')
-    #print("i am the comment: ", comments)
-    #print("i am the comment type: ", type(comments))
-
+    # print("i am the comment: ",comments)
+    # print("i am the comment type: ",type(comments))
+    
     if not comments:
         return jsonify({"error": "No comments provided"}), 400
-    
-    try:
-        #Preprocess each comment before vectoring
-        preprocessed_comments = [preprocess_comment(comment) for comment in comments]
 
-        #Transform comments using vectorizer
+    try:
+        # Preprocess each comment before vectorizing
+        preprocessed_comments = [preprocess_comment(comment) for comment in comments]
+        
+        # Transform comments using the vectorizer
         transformed_comments = vectorizer.transform(preprocessed_comments)
 
-        #convert the sparse matrix to dense format
-        dense_comments = transformed_comments.toarray() # convert to dense array
-
-        #make predictions
-        predictions = model.predict(dense_comments).tolist() # convert to list
-
-        # convert predictions to strings for consistency
+        # Convert the sparse matrix to dense format
+        dense_comments = transformed_comments.toarray()  # Convert to dense array
+        
+        # Make predictions
+        predictions = model.predict(dense_comments).tolist()  # Convert to list
+        
+        # Convert predictions to strings for consistency
         # predictions = [str(pred) for pred in predictions]
-
     except Exception as e:
-        return jsonify({"error:" f"Prediction failed: {str(e)}"}), 500
+        return jsonify({"error": f"Prediction failed: {str(e)}"}), 500
     
     # Return the response with original comments and predicted sentiments
     response = [{"comment": comment, "sentiment": sentiment} for comment, sentiment in zip(comments, predictions)]
